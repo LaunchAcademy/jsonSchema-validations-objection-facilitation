2fa86bfd05e68e289fbf0af8858801bc02b72b4c
ohad-porat <ohadp23@gmail.com>
2022-03-16 15:47:37 -0400

    import model from index.js

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,4 +1,4 @@
 // include all of your models here using CommonJS requires
 const Model = require("./Model")
 const Unicorn = require("./Unicorn")
-module.exports = { Model, Unicorn};
+module.exports = { Model, Unicorn };
